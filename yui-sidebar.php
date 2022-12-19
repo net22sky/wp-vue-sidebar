2f3814fad87714f20b21610c24ebfc4f99fca26f
net22sky <admin@i-22.ru>
2022-12-19 08:50:22 +0300

    Initial commit

---
+++ yui-sidebar.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: Мой супер плагин
+* Plugin URI: https://wordpresslab.ru/plugins/kak-sozdat-plagin-dlya-wordpress/
+* Description: Этот плагин делает мир лучше!
+* Version: 1.0.0
+* Author: WordPress студия NV3
+* Author URI: https://wordpresslab.ru/
+* License: GPL2
+*/
